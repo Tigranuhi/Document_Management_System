5f165b6ee86b0d6a6454c8c5d459842d1df37785
vgasparyan1995 <vgasparyan1995@gmail.com>
2013-11-15 15:41:44 +0300

    Update db_manager.h

--- DocMgmtSys/db_manager.h
+++ DocMgmtSys/db_manager.h
@@ -1,24 +1,50 @@
 #ifndef DOCUMENTMANAGEMENTSYSTEM_DB_MANAGER_H
 #define DOCUMENTMANAGEMENTSYSTEM_DB_MANAGER_H
 
+#include <iostream>
 #include "Query.h"
-#include <sqlite3>
+#include <string>
+#include <sqlite3.h>
 
 class db_manager
 {
 private:
-    db_manager();
+    db_manager()
+    {
+    	rc = sqlite3_open(db_path, &db);
+    }
     db_manager(const db_manager& obj);
     db_manager& operator=(const db_manager& obj);
 
     Query m_qr;
+    std::string db_path;
+    
+    //db(connection)
+    sqlite3* db;
+    char* err_msg = "";
+    int rc;
+    
+    //db_call_back function
+    int call_back(void *NotUsed, int argc, char **argv, char **azColName)
+    {
+    	//TODO
+    }
 
+    
 public:
     //singleton block
     static db_manager* s_instance = nullptr;
     static db_manager* get_instance()
     {
-        s_instance = new db_manager;
+    	if(get_permission())
+    	{
+        	s_instance = new db_manager;
+    	}
+    	else
+    	{
+    		s_instance = nullptr;
+    	}
+    	return s_instance;
     }
     static void remove_instance()
     {
@@ -28,14 +54,30 @@ public:
 
     //setter
     void set_query(const Query& qr)
-	{
-		m_qr = qr;
-	}
+    {
+    	m_qr = qr;
+    }
+    void set_db_path(const std::string& path)
+    {
+    	db_path = path;
+    }
 
     //others
+    bool get_permission()
+    {
+    	if(rc)
+    	{
+    		return false;
+    	}
+    	else
+    	{
+    		return true;
+    	}
+    }
+    
     void execute()
-	{
-
+    {
+	sqlite3_exec(db, m_qr.get_result(), call_back, 0, &err_msg);
     }
 };
 
